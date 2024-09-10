984dd3436b722fabb99c6708afcafcc77d82a62c
yoursmengle <yoursmengle@126.com>
2024-09-10 23:08:53 +0800

    update build and make script

--- make_exe.ps1
+++ make_exe.ps1
@@ -1,3 +1,8 @@
+# Check if activate.ps1 exists
+if (-Not (Test-Path -Path "venv/scripts/activate.ps1")) {
+    # If activate.ps1 does not exist, create the virtual environment
+    python -m venv venv
+}
 venv/scripts/activate.ps1
 python -m pip install -r requirements.txt
 python build.py
--- running.ps1
+++ running.ps1
@@ -1,4 +1,11 @@
-venv/scripts/activate.ps1
+# Check if activate.ps1 exists
+if (-Not (Test-Path -Path "venv/scripts/activate.ps1")) {
+    # If activate.ps1 does not exist, create the virtual environment
+    python -m venv venv
+}
+
+# Activate the virtual environment
+. venv/scripts/activate.ps1
 python -m pip install -r requirements.txt
 python main.py
 
